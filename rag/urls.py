e1734579fdaccc5d5113bac0f960dc01140c7e3f
Amera <ameraelbassal552005@gmail.com>
2025-11-27 15:45:39 +0200

    add upload pdf route to rag urls.py

--- rag/urls.py
+++ rag/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, UploadPDFView
 
 urlpatterns = [
 
-    path('api/login/', LoginView.as_view(), name='api_login'),
+    path('api/login/', LoginView.as_view()),
+    path('upload-pdf/', UploadPDFView.as_view()),
 
 ]
