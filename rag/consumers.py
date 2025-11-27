102c2ea710e6e957a545cce8c688eeec2781a0ed
Amera <ameraelbassal552005@gmail.com>
2025-11-27 22:22:37 +0200

    Retrieve response from rag even if the opanai couta ends

--- rag/consumers.py
+++ rag/consumers.py
@@ -2,35 +2,35 @@ import json
 import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .rag_logic import get_rag_response
+
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
 
-    # if not logined => refuse it
-     if self.scope["user"].is_anonymous:
-         await self.close()
-         return
-     #  accept the connection
-     await self.accept()
-     await self.send(text_data=json.dumps({
-         "type": "welcome",
-         "message": f"welcome! {self.scope['user'].username} your chat is ready :)"
-     }))
+        # if not logined => refuse it
+        if self.scope["user"].is_anonymous:
+            await self.close()
+            return
+        #  accept the connection
+        await self.accept()
+        await self.send(text_data=json.dumps({
+            "type": "welcome",
+            "message": f"welcome! {self.scope['user'].username} your chat is ready :)"
+        }))
 
-
-      # receive message from websocket
+        # receive message from websocket
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         query = data.get("message", "").strip()
         if not query:
             return
-        
+
         # Thinking message
         await self.send(text_data=json.dumps({
             "type": "thinking",
             "message": "Searching on your PDF"
         }))
-        
-        
 
-       
+    # Retrieve response from rag
+        full_response = await get_rag_response(self.scope["user"], query)
