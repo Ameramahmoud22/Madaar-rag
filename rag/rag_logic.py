c62244e772349953b2154d50b53309434dbf249f
Amera <ameraelbassal552005@gmail.com>
2025-11-27 23:45:45 +0200

    Load the FAISS vector store for this user only

--- rag/rag_logic.py
+++ rag/rag_logic.py
@@ -9,7 +9,18 @@ def get_rag_response(user, query):
     
     # the path we save on it the vectorstore for each user
     vectorstore_path = f'vectorstores/{user.username}_vectorstore'
+    
+    #check if the user has uploaded documents 
     if not os.path.exists(vectorstore_path):
         return "No documents found for this user, please upload PDFs first."
+    
+    # Load the FAISS vector store for this user only 
+    vectorstore = FAISS.load_local(
+        folder_path=vectorstore_path,
+        embeddings=OpenAIEmbeddings(),
+        allow_dangerous_deserialization=True  # must be added to upload FAISS vectorstore
+    )
+
+
 
     
