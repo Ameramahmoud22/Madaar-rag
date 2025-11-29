e7b2592f0d703059aaa1517e8cf82e52bb1f5cbc
Amera <ameraelbassal552005@gmail.com>
2025-11-29 04:11:19 +0200

    update routing on asgi file

--- rag_system/asgi.py
+++ rag_system/asgi.py
@@ -7,20 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
-import os
-from rag.middleware import TokenAuthMiddlewareStack
 import rag.routing
-
-from django.core.asgi import get_asgi_application
+from rag.middleware import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+import os
+import django
+from django.core.asgi import get_asgi_application
 
+# This is the crucial part: Set the settings module and then explicitly run django.setup()
+# This configures Django's settings and makes them available everywhere.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rag_system.settings')
+django.setup()
+
+# Now that Django is set up, we can safely import our other modules.
+
 
 application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests.
     "http": get_asgi_application(),
-    "websocket": TokenAuthMiddlewareStack(
-        URLRouter(rag.routing.websocket_urlpatterns)
 
+    # WebSocket handler
+    "websocket": TokenAuthMiddlewareStack(
+        URLRouter(
+            rag.routing.websocket_urlpatterns
+        )
     ),
 })
