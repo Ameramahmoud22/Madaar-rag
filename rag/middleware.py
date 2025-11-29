2b91f747d899a889419d304ebc800bafacf835fb
Amera <ameraelbassal552005@gmail.com>
2025-11-29 04:12:58 +0200

    add the middleware that retrieve user login token

--- rag/middleware.py
+++ rag/middleware.py
@@ -1,25 +1,39 @@
 from urllib.parse import parse_qs
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.authtoken.models import Token
+
+
+@database_sync_to_async
+def get_user_from_token(token_key):
+    """
+    Asynchronously get a user from a token key.
+    """
+    try:
+        token = Token.objects.get(key=token_key)
+        return token.user
+    except Token.DoesNotExist:
+        return AnonymousUser()
 
 
 class TokenAuthMiddleware:
+    """
+    Token authorization middleware for Django Channels.
+    """
+
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        from django.contrib.auth.models import AnonymousUser
-        from rest_framework.authtoken.models import Token
-
         qs = scope.get("query_string", b"").decode()
         params = parse_qs(qs)
-        token_keys = params.get("token")
-        if token_keys:
-            try:
-                token = Token.objects.get(key=token_keys[0])
-                scope["user"] = token.user
-            except Token.DoesNotExist:
-                scope["user"] = AnonymousUser()
+        token_key = params.get("token", [None])[0]
+
+        if token_key:
+            scope["user"] = await get_user_from_token(token_key)
         else:
             scope["user"] = AnonymousUser()
+
         return await self.inner(scope, receive, send)
 
 
